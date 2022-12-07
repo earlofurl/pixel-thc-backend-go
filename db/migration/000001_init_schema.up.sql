1c71d25263731cba8a0eed7ef61c4fff6adab655
earlofurl <jackthename@gmail.com>
2022-12-06 21:49:03 -0800

    schema edits

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,16 +1,16 @@
 -- SQL dump generated using DBML (dbml-lang.org)
 -- Database: PostgreSQL
--- Generated at: 2022-12-07T02:46:33.072Z
+-- Generated at: 2022-12-07T05:46:40.594Z
 
 CREATE TABLE "users" (
                          "id" bigserial PRIMARY KEY,
-                         "hashed_password" varchar NOT NULL,
-                         "username" varchar UNIQUE NOT NULL,
-                         "email" varchar UNIQUE NOT NULL,
-                         "first_name" varchar NOT NULL,
-                         "last_name" varchar NOT NULL,
-                         "phone" varchar NOT NULL,
-                         "role" varchar NOT NULL,
+                         "hashed_password" varchar(64) NOT NULL,
+                         "username" varchar(64) UNIQUE NOT NULL,
+                         "email" varchar(255) UNIQUE NOT NULL,
+                         "first_name" varchar(255) NOT NULL,
+                         "last_name" varchar(255) NOT NULL,
+                         "phone" varchar(26) NOT NULL,
+                         "role" varchar(255) NOT NULL,
                          "created_at" timestamptz NOT NULL DEFAULT (now()),
                          "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01',
                          "updated_at" timestamptz NOT NULL DEFAULT (now())
@@ -18,9 +18,9 @@ CREATE TABLE "users" (
 
 CREATE TABLE "accounts" (
                             "id" bigserial PRIMARY KEY,
-                            "owner" varchar NOT NULL,
+                            "owner" varchar(64) NOT NULL,
                             "balance" bigint NOT NULL,
-                            "currency" varchar NOT NULL,
+                            "currency" varchar(3) NOT NULL,
                             "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
@@ -41,10 +41,10 @@ CREATE TABLE "transfers" (
 
 CREATE TABLE "sessions" (
                             "id" uuid PRIMARY KEY,
-                            "username" varchar NOT NULL,
-                            "refresh_token" varchar NOT NULL,
-                            "user_agent" varchar NOT NULL,
-                            "client_ip" varchar NOT NULL,
+                            "username" varchar(64) NOT NULL,
+                            "refresh_token" varchar(255) NOT NULL,
+                            "user_agent" varchar(255) NOT NULL,
+                            "client_ip" varchar(45) NOT NULL,
                             "is_blocked" boolean NOT NULL DEFAULT false,
                             "expires_at" timestamptz NOT NULL,
                             "created_at" timestamptz NOT NULL DEFAULT (now())
@@ -52,7 +52,7 @@ CREATE TABLE "sessions" (
 
 CREATE TABLE "product_categories" (
                                       "id" bigserial PRIMARY KEY,
-                                      "name" varchar UNIQUE NOT NULL,
+                                      "name" varchar(255) UNIQUE NOT NULL,
                                       "created_at" timestamptz NOT NULL DEFAULT (now()),
                                       "updated_at" timestamptz NOT NULL DEFAULT (now())
 );
@@ -61,74 +61,74 @@ CREATE TABLE "strains" (
                            "id" bigserial PRIMARY KEY,
                            "created_at" timestamptz NOT NULL DEFAULT (now()),
                            "updated_at" timestamptz NOT NULL DEFAULT (now()),
-                           "name" varchar UNIQUE NOT NULL,
-                           "type" varchar NOT NULL DEFAULT 'unknown',
-                           "yield_average" decimal,
-                           "terp_average_total" decimal,
-                           "terp_1" varchar NOT NULL DEFAULT 'unknown',
-                           "terp_1_value" decimal,
-                           "terp_2" varchar NOT NULL DEFAULT 'unknown',
-                           "terp_2_value" decimal,
-                           "terp_3" varchar NOT NULL DEFAULT 'unknown',
-                           "terp_3_value" decimal,
-                           "terp_4" varchar NOT NULL DEFAULT 'unknown',
-                           "terp_4_value" decimal,
-                           "terp_5" varchar NOT NULL DEFAULT 'unknown',
-                           "terp_5_value" decimal,
-                           "thc_average" decimal,
-                           "total_cannabinoid_average" decimal,
-                           "light_dep_2022" boolean DEFAULT false,
-                           "fall_harvest_2022" boolean DEFAULT false,
-                           "quantity_available" decimal
+                           "name" varchar(255) NOT NULL,
+                           "type" varchar(255),
+                           "yield_average" numeric(9,6),
+                           "terp_average_total" numeric(9,6),
+                           "terp_1" varchar(255),
+                           "terp_1_value" numeric(9,6),
+                           "terp_2" varchar(255),
+                           "terp_2_value" numeric(9,6),
+                           "terp_3" varchar(255),
+                           "terp_3_value" numeric(9,6),
+                           "terp_4" varchar(255),
+                           "terp_4_value" numeric(9,6),
+                           "terp_5" varchar(255),
+                           "terp_5_value" numeric(9,6),
+                           "thc_average" numeric(9,6),
+                           "total_cannabinoid_average" numeric(9,6),
+                           "light_dep_2022" varchar DEFAULT 'false',
+                           "fall_harvest_2022" varchar DEFAULT 'false',
+                           "quantity_available" numeric(9,6)
 );
 
-CREATE TABLE "retail_store_locations" (
-                                          "id" bigserial PRIMARY KEY,
-                                          "created_at" timestamptz NOT NULL DEFAULT (now()),
-                                          "updated_at" timestamptz NOT NULL DEFAULT (now()),
-                                          "name" varchar UNIQUE NOT NULL,
-                                          "address" varchar NOT NULL,
-                                          "city" varchar NOT NULL,
-                                          "state" varchar NOT NULL,
-                                          "zip" integer NOT NULL,
-                                          "latitude" float,
-                                          "longitude" float,
-                                          "note" text,
-                                          "website" varchar,
-                                          "flower" boolean DEFAULT false,
-                                          "prerolls" boolean DEFAULT false,
-                                          "pressed_hash" boolean DEFAULT false,
-                                          "created_by" varchar
+CREATE TABLE "retailer_locations" (
+                                      "id" bigserial PRIMARY KEY,
+                                      "created_at" timestamptz NOT NULL DEFAULT (now()),
+                                      "updated_at" timestamptz NOT NULL DEFAULT (now()),
+                                      "name" varchar(255) UNIQUE NOT NULL,
+                                      "address" varchar(255) NOT NULL,
+                                      "city" varchar(255) NOT NULL,
+                                      "state" varchar(255) NOT NULL,
+                                      "zip" varchar(10) NOT NULL,
+                                      "latitude" numeric(9,6) NOT NULL DEFAULT 0,
+                                      "longitude" numeric(9,6) NOT NULL DEFAULT 0,
+                                      "note" varchar(1024),
+                                      "website" varchar(2083),
+                                      "sells_flower" boolean DEFAULT false,
+                                      "sells_prerolls" boolean DEFAULT false,
+                                      "sells_pressed_hash" boolean DEFAULT false,
+                                      "created_by" varchar(255)
 );
 
 CREATE TABLE "items" (
                          "id" bigserial PRIMARY KEY,
                          "created_at" timestamptz NOT NULL DEFAULT (now()),
                          "updated_at" timestamptz NOT NULL DEFAULT (now()),
-                         "description" varchar NOT NULL DEFAULT '',
+                         "description" varchar(255),
                          "is_used" boolean NOT NULL DEFAULT false,
-                         "item_type_id" bigint NOT NULL,
-                         "strain_id" bigint NOT NULL
+                         "item_type_id" bigint,
+                         "strain_id" bigint
 );
 
 CREATE TABLE "item_types" (
                               "id" bigserial PRIMARY KEY,
                               "created_at" timestamptz NOT NULL DEFAULT (now()),
                               "updated_at" timestamptz NOT NULL DEFAULT (now()),
-                              "product_form" varchar NOT NULL,
-                              "product_modifier" varchar NOT NULL,
-                              "uom_default" bigint NOT NULL,
-                              "product_category_id" bigint NOT NULL
+                              "product_form" varchar(255) NOT NULL,
+                              "product_modifier" varchar(255) NOT NULL,
+                              "uom_default" bigint,
+                              "product_category_id" bigint
 );
 
 CREATE TABLE "package_tags" (
                                 "id" bigserial PRIMARY KEY,
                                 "created_at" timestamptz NOT NULL DEFAULT (now()),
                                 "updated_at" timestamptz NOT NULL DEFAULT (now()),
-                                "tag_number" varchar NOT NULL,
-                                "is_assigned" boolean DEFAULT false,
-                                "is_provisional" boolean DEFAULT true,
-                                "is_active" boolean DEFAULT false,
+                                "tag_number" varchar(24) NOT NULL,
+                                "is_assigned" boolean NOT NULL DEFAULT false,
+                                "is_provisional" boolean NOT NULL DEFAULT true,
+                                "is_active" boolean NOT NULL DEFAULT false,
                                 "assigned_package_id" bigint
 );
 
@@ -137,36 +137,36 @@ CREATE TABLE "packages" (
                             "created_at" timestamptz NOT NULL DEFAULT (now()),
                             "updated_at" timestamptz NOT NULL DEFAULT (now()),
                             "tag_id" bigint,
-                            "package_type" varchar NOT NULL,
-                            "quantity" decimal,
-                            "notes" text,
+                            "package_type" varchar(255) NOT NULL,
+                            "quantity" numeric(9,6),
+                            "notes" varchar(1024),
                             "packaged_date_time" timestamptz NOT NULL DEFAULT (now()),
                             "harvest_date_time" timestamptz,
-                            "lab_testing_state" varchar,
+                            "lab_testing_state" varchar(255),
                             "lab_testing_state_date_time" timestamptz,
-                            "is_trade_sample" boolean DEFAULT false,
-                            "is_testing_sample" boolean DEFAULT false,
-                            "product_requires_remediation" boolean DEFAULT false,
-                            "contains_remediated_product" boolean DEFAULT false,
+                            "is_trade_sample" boolean NOT NULL DEFAULT false,
+                            "is_testing_sample" boolean NOT NULL DEFAULT false,
+                            "product_requires_remediation" boolean NOT NULL DEFAULT false,
+                            "contains_remediated_product" boolean NOT NULL DEFAULT false,
                             "remediation_date_time" timestamptz,
                             "received_date_time" timestamptz,
-                            "received_from_manifest_number" varchar,
-                            "received_from_facility_license_number" varchar,
-                            "received_from_facility_name" varchar,
-                            "is_on_hold" boolean DEFAULT false,
+                            "received_from_manifest_number" varchar(255),
+                            "received_from_facility_license_number" varchar(255),
+                            "received_from_facility_name" varchar(255),
+                            "is_on_hold" boolean NOT NULL DEFAULT false,
                             "archived_date" timestamptz,
                             "finished_date" timestamptz,
                             "item_id" bigint,
-                            "provisional_label" varchar,
-                            "is_provisional" boolean DEFAULT false,
-                            "is_sold" boolean DEFAULT false,
-                            "ppu_default" decimal,
-                            "ppu_on_order" decimal,
-                            "total_package_price_on_order" decimal,
-                            "ppu_sold_price" decimal,
-                            "total_sold_price" decimal,
-                            "packaging_supplies_consumed" boolean DEFAULT false,
-                            "is_line_item" boolean DEFAULT false,
+                            "provisional_label" varchar(255),
+                            "is_provisional" boolean NOT NULL DEFAULT false,
+                            "is_sold" boolean NOT NULL DEFAULT false,
+                            "ppu_default" numeric(19,4),
+                            "ppu_on_order" numeric(19,4),
+                            "total_package_price_on_order" numeric(19,4),
+                            "ppu_sold_price" numeric(19,4),
+                            "total_sold_price" numeric(19,4),
+                            "packaging_supplies_consumed" boolean NOT NULL DEFAULT false,
+                            "is_line_item" boolean NOT NULL DEFAULT false,
                             "order_id" bigint,
                             "uom_id" bigint
 );
@@ -180,41 +180,41 @@ CREATE TABLE "lab_tests" (
                              "id" bigserial PRIMARY KEY,
                              "created_at" timestamptz NOT NULL DEFAULT (now()),
                              "updated_at" timestamptz NOT NULL DEFAULT (now()),
-                             "test_name" varchar,
-                             "batch_code" varchar,
-                             "test_id_code" varchar,
-                             "lab_facility_name" varchar,
+                             "test_name" varchar(255),
+                             "batch_code" varchar(255),
+                             "test_id_code" varchar(255),
+                             "lab_facility_name" varchar(255),
                              "test_performed_date_time" timestamptz,
                              "overall_passed" boolean,
-                             "test_type_name" varchar,
+                             "test_type_name" varchar(255),
                              "test_passed" boolean,
-                             "test_comment" varchar,
-                             "thc_total_percent" decimal,
-                             "thc_total_value" decimal,
-                             "cbd_percent" decimal,
-                             "cbd_value" decimal,
-                             "terpene_total_percent" decimal,
-                             "terpene_total_value" decimal,
-                             "thc_a_percent" decimal,
-                             "thc_a_value" decimal,
-                             "delta9_thc_percent" decimal,
-                             "delta9_thc_value" decimal,
-                             "delta8_thc_percent" decimal,
-                             "delta8_thc_value" decimal,
-                             "thc_v_percent" decimal,
-                             "thc_v_value" decimal,
-                             "cbd_a_percent" decimal,
-                             "cbd_a_value" decimal,
-                             "cbn_percent" decimal,
-                             "cbn_value" decimal,
-                             "cbg_a_percent" decimal,
-                             "cbg_a_value" decimal,
-                             "cbg_percent" decimal,
-                             "cbg_value" decimal,
-                             "cbc_percent" decimal,
-                             "cbc_value" decimal,
-                             "total_cannabinoid_percent" decimal,
-                             "total_cannabinoid_value" decimal
+                             "test_comment" varchar(255),
+                             "thc_total_percent" numeric(9,6),
+                             "thc_total_value" numeric(9,6),
+                             "cbd_percent" numeric(9,6),
+                             "cbd_value" numeric(9,6),
+                             "terpene_total_percent" numeric(9,6),
+                             "terpene_total_value" numeric(9,6),
+                             "thc_a_percent" numeric(9,6),
+                             "thc_a_value" numeric(9,6),
+                             "delta9_thc_percent" numeric(9,6),
+                             "delta9_thc_value" numeric(9,6),
+                             "delta8_thc_percent" numeric(9,6),
+                             "delta8_thc_value" numeric(9,6),
+                             "thc_v_percent" numeric(9,6),
+                             "thc_v_value" numeric(9,6),
+                             "cbd_a_percent" numeric(9,6),
+                             "cbd_a_value" numeric(9,6),
+                             "cbn_percent" numeric(9,6),
+                             "cbn_value" numeric(9,6),
+                             "cbg_a_percent" numeric(9,6),
+                             "cbg_a_value" numeric(9,6),
+                             "cbg_percent" numeric(9,6),
+                             "cbg_value" numeric(9,6),
+                             "cbc_percent" numeric(9,6),
+                             "cbc_value" numeric(9,6),
+                             "total_cannabinoid_percent" numeric(9,6),
+                             "total_cannabinoid_value" numeric(9,6)
 );
 
 CREATE TABLE "lab_tests_packages" (
@@ -226,8 +226,8 @@ CREATE TABLE "uoms" (
                         "id" bigserial PRIMARY KEY,
                         "created_at" timestamptz NOT NULL DEFAULT (now()),
                         "updated_at" timestamptz NOT NULL DEFAULT (now()),
-                        "name" varchar UNIQUE NOT NULL,
-                        "abbreviation" varchar UNIQUE NOT NULL
+                        "name" varchar(32) NOT NULL,
+                        "abbreviation" varchar(16) NOT NULL
 );
 
 CREATE TABLE "orders" (
@@ -240,10 +240,10 @@ CREATE TABLE "orders" (
                           "actual_pack_date_time" timestamptz,
                           "actual_ship_date_time" timestamptz,
                           "actual_delivery_date_time" timestamptz,
-                          "order_total" decimal,
-                          "notes" varchar,
-                          "status" varchar NOT NULL,
-                          "customer_name" varchar NOT NULL
+                          "order_total" numeric(19,4),
+                          "notes" varchar(1024),
+                          "status" varchar(255) NOT NULL DEFAULT '',
+                          "customer_name" varchar(255) NOT NULL DEFAULT ''
 );
 
 CREATE TABLE "package_adjustments" (
